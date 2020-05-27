5f837bc42359e797d1aa9459615b2cea617978e6
John Doe <nadia643@gmail.com>
2020-05-27 10:51:16 +0100

    component did mount renders to the page

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -5,35 +5,51 @@ export default class Generator extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      loading: true,
       breed: ''
     }
+}
 
-    this.handleClick = this.handleClick.bind(this);
+  async componentDidMount() {
+    const url = 'https://catfact.ninja/breeds?limit=1';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ breed: data.data[0].breed });
+    console.log(data.data[0]);
+    
+
+
+
+    // axios.get('https://catfact.ninja/breeds?limit=1')
+    //   .then(response => {
+    //     this.setState({ breed: response.data.breed });
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+        
+    //   });
   }
-
-  handleClick = event => {
-    event.preventDefault();
-
-    axios.get('https://catfact.ninja/breeds?limit=1')
-    .then(response => {
-      console.log(response.data);
-    })
-      
-      // this.setState({ breed: response.data.country }))
-    
-    
-    
-  }
-
-
 
   render() {
     return (
       <div>
         <h1>hi from generator class</h1>
-        <button onClick={this.handleClick}>Button</button>
-        <p>{this.state.breed}</p>
+        <button onClick={this.handleClick}>Click me</button>
+        {this.state.loading ? <div>Loading....</div> : <div>breed...</div> }
+        <p>{ this.state.breed}</p>
       </div>
     )
   }
 }
+
+
+
+    // this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // handleClick = event => {
+  //   event.preventDefault();
+
+  //   axios.get('https://catfact.ninja/breeds?limit=1')
+  //   .then(response => this.setState({ breed: response.data.breed })
+  //   )
